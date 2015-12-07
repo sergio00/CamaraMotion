c6264ad287053b8ef8151c12c2780fee43045711
sergio00 <sergi_xavier01@hotmail.com>
2015-12-07 14:00:00 -0500

    camara

---
+++ CamaraWeb/CamaraWeb/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace CamaraWeb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(53, 87);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(513, 256);
+            this.videoSourcePlayer1.TabIndex = 0;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(53, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(224, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(325, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(430, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 21);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Detener";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nivel de Deteccion:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(291, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 380);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CamaraWeb/CamaraWeb/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Librerias para la inicializacion del video
+using AForge.Video.DirectShow;
+using AForge.Video;
+//Librerias para la deteccion del movimiento
+using AForge.Vision.Motion;
+
+namespace CamaraWeb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Variables para el manejo de inicializacion y finalizacion de video
+        private FilterInfoCollection dispositivos;
+        private VideoCaptureDevice fuenteDeVideo;
+        //Variables para la deteccion de movimiento
+        MotionDetector deteccion;
+        float nivelDeDeteccion;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Incializa las variables de deteccion
+            deteccion = new MotionDetector(new TwoFramesDifferenceDetector(),new MotionBorderHighlighting());
+            nivelDeDeteccion = 0;
+            //Lista de todos los dispositivos de entrada de video
+            dispositivos = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            //Cargar todo los dispositivos al comboBox
+            foreach (FilterInfo x in dispositivos)
+            {
+                comboBox1.Items.Add(x.Name);  
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Se establece el disposotivo seleccionado como fuente de video
+            fuenteDeVideo = new VideoCaptureDevice(dispositivos[comboBox1.SelectedIndex].MonikerString);
+            // Se inicializa el control de video
+            videoSourcePlayer1.VideoSource = fuenteDeVideo;
+            //Inicia la recepcion de imagenes
+            videoSourcePlayer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Para finalizar la recepcion de imagenes
+            videoSourcePlayer1.SignalToStop();
+        }
+
+        private void videoSourcePlayer1_NewFrame(object sender, ref Bitmap image)
+        {
+            nivelDeDeteccion = deteccion.ProcessFrame(image);
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //textBox1.Text = nivelDeDeteccion.ToString();
+            textBox1.Text = String.Format("{0:00.0000}",nivelDeDeteccion);
+        }
+    }
+}
